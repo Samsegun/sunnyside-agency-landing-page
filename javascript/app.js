1dab57c2105d42061771de304cd10b5f299b5a0b
Samsegun <oyebadesamsegun@gmail.com>
2021-07-14 13:10:49 -1200

    mobile responsive

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,20 @@
+// select appropriate nodes from DOM
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+const backDrop = document.querySelector(".backdrop");
 
+// action identifier
+const ACTION = "close";
+
+function mobileMenu() {
+  backDrop.classList.toggle(ACTION);
+  mobileNav.classList.toggle(ACTION);
+}
+
+toggleButton.addEventListener("click", () => {
+  mobileMenu();
+});
+
+backDrop.addEventListener("click", () => {
+  mobileMenu();
+});
